4be4b73a4d298cce4b7d2a97f98cd11b270d9c4a
Azeem Mirza <azeem.mirza94@gmail.com>
2020-05-01 15:43:21 +0500

    [feat] Command CLI Program

---
+++ index.js
@@ -0,0 +1,37 @@
+const data = require('./card.json');
+const figures = require('figures');
+const colors = require('colors');
+const log = console.log;
+
+const links = (links) => {
+    if (!Array.isArray(links)) {
+        return '';
+    }
+
+    return links.map((link) => {
+        return `\t${link.type.bold.gray}: \t${link.link}`;
+    }).join('\n');
+};
+
+const work = (work) => {
+    if (!work || !work.title || !work.company ) {
+        return '';
+    }
+
+    return `${work.title} at ${work.company}`;
+};
+
+log(`\n
+\t${data.title.america.bold}
+\t${data.subtitle.gray.bold}
+
+\t${`Work:`.bold}\t\t${work(data.work)} 
+
+\t${`Website:`.bold}\t${data.website.bold.yellow} 
+\tEmail:\t\t${data.email} 
+
+${links(data.links)}
+
+\t${`Command: \t\t`.bold.yellow}${data.command.bold.yellow}
+
+`);
